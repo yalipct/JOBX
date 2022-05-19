d227f9b8c0b23ec7da6869a930aa86acd3da4061
Aliuvys Ojeda <75907090+yalipct@users.noreply.github.com>
2022-05-19 08:10:37 +0200

    Add files via upload

--- Jobx/src/main/java/com/example/Jobx/security/enums/RoleName.java
+++ Jobx/src/main/java/com/example/Jobx/security/enums/RoleName.java
@@ -3,6 +3,25 @@ package com.example.Jobx.security.enums;
 import java.util.stream.Stream;
 
 public enum RoleName {
-    ROLE_ADMIN, ROLE_USER;
+    ROLE_ADMIN("admin"), ROLE_USER("user");
 
+    private String name;
+
+    private RoleName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static RoleName from(String value) {
+        return Stream.of(RoleName.values()).filter(targetEnum -> targetEnum.getName().equalsIgnoreCase(value))
+                .findFirst().orElse(null);
+
+    }
 }
